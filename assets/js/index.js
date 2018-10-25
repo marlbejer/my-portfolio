70c68d74c3c37d230531804b6d1923b2fd7832bd
marlbejer <marlbejer@gmail.com>
2018-10-25 16:09:42 +0800

    updated home, about, experience pages

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+	loadPage('experience');
+
+	$('.navbar-brand').click(function(){
+		loadPage('home');
+	});
+
+	$('body').on('click', 'li a', function(){
+		loadPage($(this).text().replace(' ','').toLowerCase());
+
+		$('.navbar-nav li').each(function(){
+			$(this).removeClass('active');
+		});
+
+		$(this).closest('li').addClass('active');
+
+		$('.navbar-collapse').removeClass('show');
+
+	});
+
+
+
+	function loadPage(pagetoload) {
+
+		fetch('pages/' + pagetoload + '.html')
+			.then(response => response.text())
+			.then(response => $('#main-container').html(response));
+		
+
+		
+
+
+		$("html, body").animate({ scrollTop: 0 });
+	}
+
+
+
+});
