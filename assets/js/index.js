eb83e0903e6c9cdd68833c67a8b7eeea07977125
marlbejer <marlbejer@gmail.com>
2018-10-29 10:49:18 +0800

    edited contents

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,38 @@
 $(document).ready(function(){
 
-	loadPage('home');
+	loadHomePage();
 
 	$('.navbar-brand').click(function(){
-		loadPage('home');
+		loadHomePage();
 	});
 
-	$('body').on('click', 'li a', function(){
+	$('body').on('click', '.navbar-nav li a', function(){
 		loadPage($(this).text().replace(' ','').toLowerCase());
-
 		$('.navbar-nav li').each(function(){
 			$(this).removeClass('active');
 		});
-
 		$(this).closest('li').addClass('active');
-
 		$('.navbar-collapse').removeClass('show');
 
 	});
 
-
-
-	function loadPage(pagetoload) {
-
-		fetch('pages/' + pagetoload + '.html')
-			.then(response => response.text())
-			.then(response => $('#main-container').html(response));
-		
-
-		
-
-
-		$("html, body").animate({ scrollTop: 0 });
+	function loadHomePage() {
+		loadPage('home');
+		$('.navbar-nav li').each(function(){
+			$(this).removeClass('active');
+		});
+		$('.navbar-nav li:first-child').addClass('active');
 	}
 
-
+	function loadPage(pagetoload) {
+		let loadIfHasError = (window.location.href).split('#')[1];
+		fetch('pages/' + pagetoload + '.html')
+			.then(response => response.text())
+			.then(response => $('#main-container').html(response))
+			.catch(function(error){
+				loadHomePage(loadIfHasError);
+			});
+		$("html, body").animate({ scrollTop: 0 });
+	}
 
 });
